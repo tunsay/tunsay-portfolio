325b8500bce4128d2a2c92341444295b682f9902
tunsay <tuna1997@hotmail.fr>
2023-11-23 02:33:12 +0100

    update header

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <div className="header-bar">
+      <header>
+        <div className="personal-info">
+          <h1>Tuna Saygili</h1>
+          <h2>Développeur Full stack</h2>
+        </div>
+        <nav className="navigation-links">
+          <a href="#competences">Compétences</a>
+          <a href="#projets">Projets</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Header
