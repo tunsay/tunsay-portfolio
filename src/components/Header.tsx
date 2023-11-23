7372d6dbffa8119b22ee640011ab51f2bd6e6491
tunsay <tuna1997@hotmail.fr>
2023-11-23 20:09:06 +0100

    update herader

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 function Header() {
   return (
     <div className="header-bar">
+      <div className="progress"></div>
       <header>
         <div className="personal-info">
           <h1>Tuna Saygili</h1>
           <h2>Développeur Full stack</h2>
         </div>
         <nav className="navigation-links">
-          <a href="#competences">Compétences</a>
-          <a href="#projets">Projets</a>
+          <a href="#skills">Compétences</a>
+          <a href="#projects">Projets</a>
           <a href="#contact">Contact</a>
         </nav>
       </header>
