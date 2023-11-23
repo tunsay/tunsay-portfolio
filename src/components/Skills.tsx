752894972d9bbfbbc2e2285cbdf6758ec1846620
tunsay <tuna1997@hotmail.fr>
2023-11-23 02:33:25 +0100

    add skills comp

---
+++ src/components/Skills.tsx
@@ -0,0 +1,12 @@
+function Skills() {
+  return (
+    <div className="container-skills">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quis
+      possimus adipisci corrupti? Sint asperiores similique velit dolore
+      molestiae rem, architecto voluptatem eos fugit recusandae autem magnam?
+      Odit, voluptate dignissimos.
+    </div>
+  )
+}
+
+export default Skills
