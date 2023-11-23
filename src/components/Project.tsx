2ac5264adeaae6585bf9984474f05f70a7711c28
tunsay <tuna1997@hotmail.fr>
2023-11-23 02:33:46 +0100

    add project comp

---
+++ src/components/Project.tsx
@@ -0,0 +1,12 @@
+function Project() {
+  return (
+    <div className="container-project">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio quas harum
+      tenetur laborum nesciunt? Ad, voluptates? Eligendi, quae perspiciatis.
+      Minus neque perspiciatis ea ab temporibus similique impedit? Ratione,
+      molestias rem!
+    </div>
+  )
+}
+
+export default Project
