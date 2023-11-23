d4384e79c4e67a3eeb995cd0ebb4374455e36018
tunsay <tuna1997@hotmail.fr>
2023-11-23 02:33:39 +0100

    add home comp

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="container-Home">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores impedit
+      error eius eveniet alias nostrum. Laboriosam, quasi qui. Possimus pariatur
+      consectetur veniam nihil dolorum similique voluptatibus voluptatem
+      repellat quasi qui.
+    </div>
+  )
+}
+
+export default Home
