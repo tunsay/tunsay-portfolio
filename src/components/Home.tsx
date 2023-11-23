f037c52a94f6e8ff163c1dc7caf5d70ae029b890
tunsay <tuna1997@hotmail.fr>
2023-11-23 20:09:16 +0100

    uodate home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,8 @@
+import styles from './Main.module.scss'
+
 function Home() {
   return (
-    <div className="container-Home">
+    <div className={styles.home__container}>
       tLorem ipsum dolor sit amet consectetur adipisicing elit. Dolores impedit
       error eius eveniet alias nostrum. Laboriosam, quasi qui. Possimus pariatur
       consectetur veniam nihil dolorum similique voluptatibus voluptatem
