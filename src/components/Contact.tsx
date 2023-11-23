a5dd3ce383b7756ddc78a0159643c2d717eb6c8a
tunsay <tuna1997@hotmail.fr>
2023-11-23 02:33:30 +0100

    add contact comp

---
+++ src/components/Contact.tsx
@@ -0,0 +1,12 @@
+function Contact() {
+  return (
+    <div className="container-contact">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores impedit
+      error eius eveniet alias nostrum. Laboriosam, quasi qui. Possimus pariatur
+      consectetur veniam nihil dolorum similique voluptatibus voluptatem
+      repellat quasi qui.
+    </div>
+  )
+}
+
+export default Contact
