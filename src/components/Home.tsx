47b9efae69dc3215d54439a54cfc3b43335bbb7e
tunsay <tuna1997@hotmail.fr>
2023-11-25 19:00:25 +0100

    update Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,20 @@
-import styles from './Main.module.scss'
+import './Home.css'
+import avatar from '../assets/avatar.png'
 
 function Home() {
   return (
-    <div className={styles.home__container}>
-      tLorem ipsum dolor sit amet consectetur adipisicing elit. Dolores impedit
-      error eius eveniet alias nostrum. Laboriosam, quasi qui. Possimus pariatur
-      consectetur veniam nihil dolorum similique voluptatibus voluptatem
-      repellat quasi qui.
+    <div className="home-container">
+      <div className="home-avatar">
+        <img src={avatar} alt="avatar qui ressemble à Touna" />
+      </div>
+      <div className="home-about">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio ratione
+        repudiandae voluptas ducimus neque enim. Ex, quod rerum culpa blanditiis
+        accusamus maiores quidem odio recusandae sed minima porro aut vitae
+        placeat laborum corporis provident fugiat animi nisi id. Ex minus
+        possimus nemo, adipisci sed obcaecati eligendi assumenda exercitationem
+        eos maxime!
+      </div>
     </div>
   )
 }
