b5a16c64f440b753191bfbeee7c9b825d34c7eca
tunsay <tuna1997@hotmail.fr>
2023-11-25 19:00:31 +0100

    update main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,20 +3,21 @@ import Home from './Home'
 import Project from './Project'
 import Skills from './Skills'
 import styles from './Main.module.scss'
+import './Home.css'
 
 function Main() {
   return (
     <>
       <main className="main">
         {/* Sections */}
-        <section className={styles.home} id="home">
+        <section className="home" id="home">
           <Home />
-        </section>
-        <section className={styles.skills} id="skills">
-          <Skills />
         </section>
         <section className={styles.projects} id="projects">
           <Project />
+        </section>
+        <section className={styles.skills} id="skills">
+          <Skills />
         </section>
         <section className={styles.contact} id="contact">
           <Contact />
