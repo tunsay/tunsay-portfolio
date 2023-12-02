916560edc96d13f418460075ea20d9a4c2c3de21
tunsay <tuna1997@hotmail.fr>
2023-12-02 02:15:11 +0100

    update project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,9 +1,29 @@
-import './Project.css'
+import './Project.scss'
 
 function Project() {
   return (
     <div className="container-project">
       <h1 className="title-container">Projets</h1>
+      <div className="card-scroll-wrap">
+        <div className="intro-card">
+          <p>
+            A glargregregregnstrate the <strong>Glassmorphism UI design</strong>{' '}
+            trend.
+          </p>
+        </div>
+        <div className="intro-card sticky-1">
+          <p>
+            A glass-likegregregregregreghe{' '}
+            <strong>Glassmorphism UI design</strong> trend.
+          </p>
+        </div>
+        <div className="intro-card sticky-2">
+          <p>
+            A glass-lfezfezfezfezfate the{' '}
+            <strong>Glassmorphism UI design</strong> trend.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
