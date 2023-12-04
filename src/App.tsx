a404d5b2850eac3ae03f069606857d3f7d0739b1
tunsay <tuna1997@hotmail.fr>
2023-12-04 22:09:26 +0100

    Change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import './components/Header.css'
 /* router */
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ParallaxProvider } from 'react-scroll-parallax'
 import Main from './components/Main'
 import Header from './components/Header'
 
@@ -10,9 +10,11 @@ function App() {
     <React.Fragment>
       <BrowserRouter basename="/tunsay-portfolio">
         <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
+        <ParallaxProvider>
+          <Routes>
+            <Route path="/" element={<Main />} />
+          </Routes>
+        </ParallaxProvider>
       </BrowserRouter>
     </React.Fragment>
   )
