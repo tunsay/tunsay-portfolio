f8673f3d367b0b90a6aa251befe368423127e48f
tunsay <tuna1997@hotmail.fr>
2023-12-06 21:27:23 +0100

    update skills scss

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,7 +4,8 @@ function Skills() {
   return (
     <div className="container-skills">
       <h1 className="title-container">Compétences</h1>
-      <div className="grids">
+      <h2 className="title-skills">Technologies Web</h2>
+      <div className="grids techno-web">
         <div className="item">
           <div className="box">
             <div className="percent">
@@ -22,6 +23,25 @@ function Skills() {
               </div>
             </div>
             <h2>Html</h2>
+          </div>
+        </div>
+        <div className="item">
+          <div className="box">
+            <div className="percent">
+              <svg>
+                <circle cx="70" cy="70" r="70"></circle>
+                <circle cx="70" cy="70" r="70"></circle>
+              </svg>
+              <div className="num">
+                <img
+                  width="96"
+                  height="96"
+                  src="https://img.icons8.com/color/96/css3.png"
+                  alt="css3"
+                />
+              </div>
+            </div>
+            <h2>css</h2>
           </div>
         </div>
         <div className="item">
@@ -111,12 +131,148 @@ function Skills() {
                 <img
                   width="96"
                   height="96"
+                  src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-redux-an-open-source-javascript-library-for-managing-application-state-logo-color-tal-revivo.png"
+                  alt="external-redux-an-open-source-javascript-library-for-managing-application-state-logo-color-tal-revivo"
+                />
+              </div>
+            </div>
+            <h2>redux</h2>
+          </div>
+        </div>
+        <div className="item">
+          <div className="box">
+            <div className="percent">
+              <svg>
+                <circle cx="70" cy="70" r="70"></circle>
+                <circle cx="70" cy="70" r="70"></circle>
+              </svg>
+              <div className="num">
+                <img
+                  width="96"
+                  height="96"
                   src="https://img.icons8.com/nolan/96/symfony.png"
                   alt="symfony"
                 />
               </div>
             </div>
             <h2>symfony</h2>
+          </div>
+        </div>
+      </div>
+      <h2 className="title-skills">Outils Web</h2>
+      <div className="grids tools-web">
+        <div className="item">
+          <div className="box">
+            <div className="percent">
+              <svg>
+                <circle cx="70" cy="70" r="70"></circle>
+                <circle cx="70" cy="70" r="70"></circle>
+              </svg>
+              <div className="num">
+                <img
+                  width="96"
+                  height="96"
+                  src="https://img.icons8.com/pulsar-color/96/github.png"
+                  alt="github"
+                />
+              </div>
+            </div>
+            <h2>GitHub</h2>
+          </div>
+        </div>
+        <div className="item">
+          <div className="box">
+            <div className="percent">
+              <svg>
+                <circle cx="70" cy="70" r="70"></circle>
+                <circle cx="70" cy="70" r="70"></circle>
+              </svg>
+              <div className="num">
+                <img
+                  width="96"
+                  height="96"
+                  src="https://img.icons8.com/external-those-icons-flat-those-icons/96/external-Bootstrap-Logo-social-media-those-icons-flat-those-icons.png"
+                  alt="external-Bootstrap-Logo-social-media-those-icons-flat-those-icons"
+                />
+              </div>
+            </div>
+            <h2>bootsrap</h2>
+          </div>
+        </div>
+        <div className="item">
+          <div className="box">
+            <div className="percent">
+              <svg>
+                <circle cx="70" cy="70" r="70"></circle>
+                <circle cx="70" cy="70" r="70"></circle>
+              </svg>
+              <div className="num">
+                <img
+                  width="64"
+                  height="64"
+                  src="https://img.icons8.com/dusk/64/api-settings.png"
+                  alt="api-settings"
+                />
+              </div>
+            </div>
+            <h2>api</h2>
+          </div>
+        </div>
+        <div className="item">
+          <div className="box">
+            <div className="percent">
+              <svg>
+                <circle cx="70" cy="70" r="70"></circle>
+                <circle cx="70" cy="70" r="70"></circle>
+              </svg>
+              <div className="num">
+                <img
+                  width="96"
+                  height="96"
+                  src="https://img.icons8.com/fluency/96/visual-studio-code-2019.png"
+                  alt="visual-studio-code-2019"
+                />
+              </div>
+            </div>
+            <h2>vscode</h2>
+          </div>
+        </div>
+        <div className="item">
+          <div className="box">
+            <div className="percent">
+              <svg>
+                <circle cx="70" cy="70" r="70"></circle>
+                <circle cx="70" cy="70" r="70"></circle>
+              </svg>
+              <div className="num">
+                <img
+                  width="96"
+                  height="96"
+                  src="https://img.icons8.com/pulsar-color/96/figma.png"
+                  alt="figma"
+                />
+              </div>
+            </div>
+            <h2>figma</h2>
+          </div>
+        </div>
+        <div className="item">
+          <div className="box">
+            <div className="percent">
+              <svg>
+                <circle cx="70" cy="70" r="70"></circle>
+                <circle cx="70" cy="70" r="70"></circle>
+              </svg>
+              <div className="num">
+                <img
+                  width="96"
+                  height="96"
+                  src="https://img.icons8.com/color/96/stack.png"
+                  alt="stack"
+                />
+              </div>
+            </div>
+            <h2>mvc</h2>
           </div>
         </div>
       </div>
