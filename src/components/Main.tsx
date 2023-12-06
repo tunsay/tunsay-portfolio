986fbfc01989a93430c640961d1a0fd4bc84408c
tunsay <tuna1997@hotmail.fr>
2023-12-06 21:27:09 +0100

    update main tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,6 +8,10 @@ function Main() {
   return (
     <>
       <main className="main">
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+        />
         {/* Sections */}
         <section className="home" id="home">
           <Home />
