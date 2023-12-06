e64bddb4774991f2bef12d98575cda70bec8ed20
tunsay <tuna1997@hotmail.fr>
2023-12-06 21:26:52 +0100

    update home tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,40 +5,108 @@ function Home() {
   return (
     <div className="home-container">
       <div className="home-left">
-        <Parallax speed={10} translateY={['100px', '-100px']}>
-          <div className="home-left-infos">
-            <a
-              href="../assets/cv-tuna-saygili.pdf"
-              className="cv"
-              target="_blank"
-              title="Encore un peu plus de moi."
-            >
-              <div className="cv__link">CV</div>
-            </a>
-            <div className="profil"></div>
-          </div>
-          {/* <img src={avatar} alt="avatar qui ressemble à Touna" /> */}
-        </Parallax>
+        <div className="home-left-infos">
+          <a
+            href="../assets/cv-tuna-saygili.pdf"
+            className="cv"
+            target="_blank"
+            title="Encore un peu plus de moi."
+          >
+            <div className="cv__link">CV</div>
+          </a>
+          <div className="profil"></div>
+        </div>
+        {/* <img src={avatar} alt="avatar qui ressemble à Touna" /> */}
       </div>
       <div className="home-right">
         <Parallax speed={10} translateY={['-100px', '100px']}>
           <span className="about-absolute">about</span>
           <h1 className="title-perso">Salut! C'est Tuna !</h1>
           <div className="desc-perso">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio
-            ratione repudiandae voluptas ducimus neque enim. Ex, quod rerum
-            culpa blanditiis accusamus maiores quidem odio recusandae sed minima
-            porro aut vitae placeat laborum corporis provident fugiat animi nisi
-            id. Ex minus possimus nemo, adipisci sed obcaecati eligendi
-            assumenda exercitationem eos maxime!
+            <p>
+              Ma détermination et ma passion me poussent à développer mes
+              créations avec une attention particulière à l'expérience
+              utilisateur. Animé par une soif d'apprendre constante, je suis à
+              la recherche d'un emploi qui me permettra de progresser. Je suis
+              capable de créer, maquetter et développer le Front et le Back d'un
+              projet, grâce à ma multifonctionnalité.
+            </p>
           </div>
           <div className="social-perso"></div>
-          <div className="contact-perso">
-            <h2>Contact Moi !</h2>
-            <p className="mail">Email: tuna.saygili@live.fr</p>
+          <h2 className="title-perso">Contact Moi !</h2>
+          <div className="mail-circle">
+            <span className="my-mail">tuna.saygili@live.fr</span>
+            <div className="circle">
+              <span className="material-symbols-outlined mail-icon">mail</span>
+            </div>
           </div>
+          {/* <p>Email: tuna.saygili@live.fr</p> */}
         </Parallax>
       </div>
+      <p className="marquee">
+        <span>
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test -
+          Dynamique - Motivé - Curieux - Passioné - Investi - Perfectioniste -
+          Javascript - React - Redux - Metal Gear Solid - Zelda - Test - &nbsp;
+        </span>
+      </p>
     </div>
   )
 }
