feab1e360f90a9d47ea1aca71f55a9ed203f3cfb
tunsay <tuna1997@hotmail.fr>
2023-12-07 21:33:11 +0100

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>@ Copyright Reserved 2024, Site By Tuna Saygili</p>
+    </div>
+  )
+}
+
+export default Footer
