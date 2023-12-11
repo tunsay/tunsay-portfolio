2807066566cfed7ad964ec3f8c7898d83103a17f
tunsay <tuna1997@hotmail.fr>
2023-12-11 22:19:32 +0100

    update contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,19 @@
+import { faMailBulk, faPhone } from '@fortawesome/free-solid-svg-icons'
 import './Contact.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Contact() {
   return (
     <div className="container-contact">
       <div className="title-container">Contact</div>
+      <a className="mail" href="mailto:tuna.saygili@live.fr?">
+        <FontAwesomeIcon icon={faMailBulk} className="icon-mail" />
+        <span>tuna.saygili@live.fr</span>
+      </a>
+      <span className="num">
+        <FontAwesomeIcon icon={faPhone} className="icon-mail" />
+        <span>06.95.13.66.29</span>
+      </span>
     </div>
   )
 }
