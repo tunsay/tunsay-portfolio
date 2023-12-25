71e7ed3fea11f8f0ded59e53a78a8db8f57e49ad
tunsay <tuna1997@hotmail.fr>
2023-12-25 17:06:06 +0100

    add ghpages button

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,12 +8,14 @@ import petitPlatLogo from '../assets/logo3.png'
 import sportSeeLogo from '../assets/logo4.svg'
 import kasaLogo from '../assets/logo5.png'
 import { Parallax } from 'react-scroll-parallax'
+import { useState } from 'react'
 
 interface ProjectItemProps {
   mockUpImage: string
   logoImage: string
   description: string
-  url: string
+  urlGit: string
+  urlGhPages: string
 }
 
 const projectItems = [
@@ -22,28 +24,32 @@ const projectItems = [
     logoImage: argentBankLogo,
     description:
       "Le projet consiste à développer une application web React pour Argent Bank, avec une première phase axée sur l'authentification des utilisateurs. Les outils principaux incluent React pour la construction de l'interface utilisateur, Redux pour la gestion de l'état global de l'application, et Swagger pour spécifier les endpoints d'API nécessaires à la communication avec le backend. L'équipe devra également modéliser les API pour la future phase 2 du projet, dédiée aux transactions.",
-    url: 'https://github.com/tunsay/P13_Argent_Bank_Front',
+    urlGit: 'https://github.com/tunsay/P13_Argent_Bank_Front',
+    urlGhPages: '',
   },
   {
     mockUpImage: petitPlatMockUp,
     logoImage: petitPlatLogo,
     description:
       "L'entreprise, spécialisée dans les livres de cuisine, se lance dans la création d'un site de recettes. La première mission consiste à mettre en œuvre une fonction de recherche efficace à l'aide d'un fichier JSON de 50 recettes, en utilisant des technologies front-end comme React et des outils tels que Git et Jsben.ch pour comparer les performances de deux implémentations distinctes.",
-    url: 'https://github.com/tunsay/P7_Les-petits-plats',
+    urlGit: 'https://github.com/tunsay/P7_Les-petits-plats',
+    urlGhPages: 'https://tunsay.github.io/P7_Les-petits-plats/',
   },
   {
     mockUpImage: sportSeeMockUp,
     logoImage: sportSeeLogo,
     description:
       "SportSee lance une nouvelle version de la page profil utilisateur, focalisée sur le suivi des sessions et des calories. Le développement de la page avec React inclut l'intégration de graphiques, avec le choix entre D3 et Recharts. Le backend NodeJS fournit des données d'exemple via des appels HTTP, à gérer hors des composants React avec Fetch ou Axios, et nécessite une standardisation des données de l'API.",
-    url: 'https://github.com/tunsay/SportSee',
+    urlGit: 'https://github.com/tunsay/SportSee',
+    urlGhPages: 'https://tunsay.github.io/P12-front-end-dashboard/',
   },
   {
     mockUpImage: kasaMockUp,
     logoImage: kasaLogo,
     description:
       "Kasa entreprend une refonte majeure de son site ASP.NET vieux de 10 ans vers une stack JavaScript (NodeJS, React). La mission consiste à démarrer le projet React, développer l'application front-end en suivant les maquettes Figma, avec des données d'exemple fournies en attendant le recrutement du back-end. Le design détaillé par Paul inclut des fonctionnalités comme une galerie d'images réactive et des Collapse.",
-    url: 'https://github.com/tunsay/p11_my_project_kasa',
+    urlGit: 'https://github.com/tunsay/p11_my_project_kasa',
+    urlGhPages: 'https://tunsay.github.io/p11_my_project_kasa/',
   },
 ]
 
@@ -51,7 +57,8 @@ function ProjectItem({
   mockUpImage,
   logoImage,
   description,
-  url,
+  urlGit,
+  urlGhPages,
 }: ProjectItemProps) {
   return (
     <div className="project-item">
@@ -69,14 +76,26 @@ function ProjectItem({
         <Parallax translateY={['30px', '0px']} easing={[0, 1, 1, 1]}>
           <div className="desc">{description}</div>
         </Parallax>
-        <a
-          className="link-github"
-          href={url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Voir Projet
-        </a>
+        <div className="link-row">
+          <a
+            className="link-github"
+            href={urlGit}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Voir Repo
+          </a>
+          {urlGhPages && (
+            <a
+              className="link-github"
+              href={urlGhPages}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Voir Projet
+            </a>
+          )}
+        </div>
       </div>
     </div>
   )
@@ -92,7 +111,8 @@ function Project() {
           mockUpImage={item.mockUpImage}
           logoImage={item.logoImage}
           description={item.description}
-          url={item.url}
+          urlGit={item.urlGit}
+          urlGhPages={item.urlGhPages}
         />
       ))}
     </div>
