5290fbdcaf312259587878d79bce3252a06d8a3e
tunsay <tuna1997@hotmail.fr>
2026-05-12 15:38:54 +0200

    feat: add Turnstile verification to Contact component and remove App.test.tsx - Integrated @marsidev/react-turnstile for user verification in the Contact component. - Conditional rendering based on verification status to display contact information. - Removed obsolete App.test.tsx file. - Updated package.json to include the new Turnstile dependency.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,19 +1,35 @@
+import { useState } from 'react'
 import { faMailBulk, faPhone } from '@fortawesome/free-solid-svg-icons'
 import './Contact.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Turnstile } from '@marsidev/react-turnstile'
 
 function Contact() {
+  const [isVerified, setIsVerified] = useState(false)
+
   return (
     <div className="container-contact">
       <div className="title-container">Contact</div>
-      <a className="mail" href="mailto:tuna.saygili@live.fr?">
-        <FontAwesomeIcon icon={faMailBulk} className="icon-mail" />
-        <span>tuna.saygili@live.fr</span>
-      </a>
-      <span className="num">
-        <FontAwesomeIcon icon={faPhone} className="icon-mail" />
-        <span>06.95.13.66.29</span>
-      </span>
+
+      {!isVerified ? (
+        <div className="verification-container" style={{ margin: '20px 0', display: 'flex', justifyContent: 'center' }}>
+          <Turnstile
+            siteKey="0x4AAAAAADN5wKHfgkr53iiq"
+            onSuccess={() => setIsVerified(true)}
+          />
+        </div>
+      ) : (
+        <>
+          <a className="mail" href="mailto:tuna.saygili@live.fr?">
+            <FontAwesomeIcon icon={faMailBulk} className="icon-mail" />
+            <span>tuna.saygili@live.fr</span>
+          </a>
+          <span className="num">
+            <FontAwesomeIcon icon={faPhone} className="icon-mail" />
+            <span>06.95.13.66.29</span>
+          </span>
+        </>
+      )}
     </div>
   )
 }
